709cca669086836e3a736d64536c0c0d75da591a
Dmitry Uchkin <125159427+wakeuphaku@users.noreply.github.com>
2025-06-06 00:56:23 +0300

    fix size

--- plugins/aerospace.sh
+++ plugins/aerospace.sh
@@ -3,19 +3,19 @@
 # make sure it's executable with:
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
-FOCUS_COLOR=0xccb4befe    # Активный: насыщенный фиолетовый
-NORMAL_COLOR=0x33b4befe   # Неактивный: прозрачный "стекло"
+FOCUS_COLOR=0xccb4befe  # Активный: насыщенный фиолетовый
+NORMAL_COLOR=0x33b4befe # Неактивный: прозрачный "стекло"
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
   sketchybar --set $NAME \
     background.color=$FOCUS_COLOR \
     label.shadow.drawing=off \
     icon.shadow.drawing=off \
-    background.border_width=2
+    background.border_width=0
 else
   sketchybar --set $NAME \
     background.color=$NORMAL_COLOR \
     label.shadow.drawing=off \
     icon.shadow.drawing=off \
-    background.border_width=0
+    background.border_width=2
 fi
